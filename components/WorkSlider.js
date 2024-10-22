50a9e006b02806d4dbc7f24157968c71de5368d1
Zulnurain <meet.zulnurain@gmail.com>
2024-10-22 05:28:13 -0700

    Update WorkSlider.js

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -6,18 +6,22 @@ const workSlides = {
         {
           title: 'title',
           path: '/thumb1.jpg',
+          link: 'https://flasholr.com/',
         },
         {
           title: 'title',
           path: '/thumb2.jpg',
+          link: 'https://ya-hub.com/',
         },
         {
           title: 'title',
           path: '/thumb3.jpg',
+          link: 'https://gotarepair.com/',
         },
         {
           title: 'title',
           path: '/thumb4.jpg',
+          link: '',
         },
       ],
     },
@@ -53,6 +57,7 @@ import 'swiper/css/pagination'
 import Image from 'next/image';
 import {Pagination} from 'swiper'
 import {BsArrowRight} from 'react-icons/bs'
+import Link from 'next/link';
 
 
 const WorkSlider = () => {
@@ -72,17 +77,19 @@ const WorkSlider = () => {
             {slide.images.map((image,index)=>{
               return (
                 <div className='relative rounded-lg overflow-hidden flex items-center justify-center group' key={index}>
-                  <div className='flex items-center justify-center relative overflow-hidden group'>
-                    <Image src={image.path} width={400} height={300} alt='' />
-                    <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
-                    <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 duration-300'>
-                      <div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
-                       <div className='delay-100'>LIVE</div>
-                       <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>PROJECT</div>
-                       <div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'><BsArrowRight /></div>
+                    <Link href={image.link ? image.link : '/'} >
+                      <div className='flex items-center justify-center relative overflow-hidden group'>
+                        <Image src={image.path} width={400} height={300} alt='' />
+                        <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
+                        <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 duration-300'>
+                          <div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
+                          <div className='delay-100'>LIVE</div>
+                          <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>PROJECT</div>
+                          <div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'><BsArrowRight /></div>
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </div>
+                    </Link>
                 </div>
               )
             })}
